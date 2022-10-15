1a6ac1f63198d07c1432efbb1591f62088600142
antonio.mykonios <conexaovirtual_net@hotmail.com>
2022-10-15 20:47:52 -0300

    #Layout Login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,10 +22,9 @@
             <!-- Authentication Links -->
             @guest
                 <!-- nao logado -->
-                <!--
-                    <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                    <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                -->
+                <!--<li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>-->
+                <!--<li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>-->
+                
             @else
                 <!-- logado -->
                 <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
