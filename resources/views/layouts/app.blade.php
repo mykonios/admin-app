6f99e1437e76e64986859826b5b37cb772794251
antonio.mykonios <conexaovirtual_net@hotmail.com>
2022-10-16 16:34:44 -0300

    #Layout password view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1245,7 +1245,6 @@
     <script src="{{ asset('js/bundle19ce.js?ver=3.0.3') }}></script>
     <script src="{{ asset('js/scripts19ce.js?ver=3.0.3') }}></script>
     <script src="{{ asset('js/demo-settings19ce.js?ver=3.0.3') }}></script>
-    <script src="{{ asset('js/charts/chart-ecommerce19ce.js?ver=3.0.3') }}></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>  
+    <script src="{{ asset('js/charts/chart-ecommerce19ce.js?ver=3.0.3') }}></script> 
  </body>
 </html>
