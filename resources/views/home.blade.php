151c23251cd6e60187f38e941cfdf819b41cfad1
antonio.mykonios <conexaovirtual_net@hotmail.com>
2022-10-16 11:11:49 -0300

    #Layout content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in aeeeeeee!') }}
                 </div>
             </div>
         </div>
