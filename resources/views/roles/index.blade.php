21c05da72ff0c3f92ed358807f9c300c0512370e
antonio.mykonios <conexaovirtual_net@hotmail.com>
2022-10-17 18:26:17 -0300

    #Layout permissões

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,97 +1,75 @@
 @extends('layouts.app')
 
 @section('content')
-<table class="table table-bordered">
-    <tr>
-        <th>No</th>
-        <th>Name</th>
-        <th width="280px">Action</th>
-    </tr>
-    
-    @foreach ($roles as $key => $role)
-    <tr>
-        <td>{{ ++$i }}</td>
-        <td>{{ $role->name }}</td>
-        <td>
-            <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
-            @can('role-edit')
-                <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
-            @endcan
-            @can('role-delete')
-                {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                {!! Form::close() !!}
-            @endcan
-        </td>
-    </tr>
-    @endforeach
-</table>
 
-<div class="nk-content" style="margin-top: 85px;">
-    <div class="container-fluid">
-        <div class="nk-content-body">
+@can('role-edit')
+    <div class="nk-content" style="margin-top: 85px;">
+        <div class="container-fluid">
+            <div class="nk-content-body">
 
 
-            <div class="nk-block-head nk-block-head-sm">
-                <div class="nk-block-between">
-                    <div class="nk-block-head-content">
-                        <h3 class="nk-block-title page-title">Permissões</h3>
-                    </div>
-                    <div class="nk-block-head-content">
-                        <div class="toggle-wrap nk-block-tools-toggle">
-                            <a href="#" class="btn btn-icon btn-trigger toggle-expand me-n1" data-target="pageMenu">
-                                <em class="icon ni ni-more-v"></em>
-                            </a>
-                            <div class="toggle-expand-content" data-content="pageMenu">
-                                @can('product-create')
-                                    <a class="btn btn-success" href="{{ route('roles.create') }}"> Nova Permissão</a>
-                                @endcan
+                <div class="nk-block-head nk-block-head-sm">
+                    <div class="nk-block-between">
+                        <div class="nk-block-head-content">
+                            <h3 class="nk-block-title page-title">Permissões</h3>
+                        </div>
+                        <div class="nk-block-head-content">
+                            <div class="toggle-wrap nk-block-tools-toggle">
+                                <a href="#" class="btn btn-icon btn-trigger toggle-expand me-n1" data-target="pageMenu">
+                                    <em class="icon ni ni-more-v"></em>
+                                </a>
+                                <div class="toggle-expand-content" data-content="pageMenu">
+                                    @can('product-create')
+                                        <a class="btn btn-success" href="{{ route('roles.create') }}"> Nova Permissão</a>
+                                    @endcan
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="nk-block">
-                <div class="row g-gs">
+                <div class="nk-block">
+                    <div class="row g-gs">
 
-                    @if ($message = Session::get('success'))
-                        <div class="alert alert-success">
-                            <p>{{ $message }}</p>
-                        </div>
-                    @endif
+                        @if ($message = Session::get('success'))
+                            <div class="alert alert-success">
+                                <p>{{ $message }}</p>
+                            </div>
+                        @endif
 
-                    <table class="table table-bordered">
-                        <tr>
-                            <th>No</th>
-                            <th>Name</th>
-                            <th width="280px">Action</th>
-                        </tr>
-                        
-                        @foreach ($roles as $key => $role)
-                        <tr>
-                            <td>{{ ++$i }}</td>
-                            <td>{{ $role->name }}</td>
-                            <td>
-                                <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
-                                @can('role-edit')
-                                    <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
-                                @endcan
-                                @can('role-delete')
-                                    {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
-                                        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                                    {!! Form::close() !!}
-                                @endcan
-                            </td>
-                        </tr>
-                        @endforeach
-                    </table>
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>No</th>
+                                <th>Name</th>
+                                <th width="280px">Action</th>
+                            </tr>
+                            
+                            @foreach ($roles as $key => $role)
+                            <tr>
+                                <td>{{ ++$i }}</td>
+                                <td>{{ $role->name }}</td>
+                                <td>
+                                    <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
+                                    @can('role-edit')
+                                        <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
+                                    @endcan
+                                    @can('role-delete')
+                                        {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
+                                            {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                                        {!! Form::close() !!}
+                                    @endcan
+                                </td>
+                            </tr>
+                            @endforeach
+                        </table>
 
-                </div>        
-            </div>
-        </div>     
-    </div>    
-</div>
-
+                    </div>        
+                </div>
+            </div>     
+        </div>    
+    </div>
+@else
+    testeee
+@endcan
 {!! $roles->render() !!}
 @endsection
