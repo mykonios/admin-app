b3e386cda2248c098d12b0a2dd5bbbd120352b25
antonio.mykonios <conexaovirtual_net@hotmail.com>
2022-10-17 16:36:47 -0300

    #Layout usuários

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -30,20 +30,6 @@
             <div class="nk-block">
                 <div class="row g-gs">
 
-                    <!--<div class="row">
-                        <div class="col-lg-12 margin-tb">
-                            <div class="pull-left">
-                                <h2>Products</h2>
-                            </div>
-                            <div class="pull-right">
-                                @can('product-create')
-                                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
-                                @endcan
-                            </div>
-                        </div>
-                    </div>-->
-
-
                     @if ($message = Session::get('success'))
                         <div class="alert alert-success">
                             <p>{{ $message }}</p>
