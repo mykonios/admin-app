e9c6096966b39d27b29fbca29a9d5f55b8936419
antonio.mykonios <conexaovirtual_net@hotmail.com>
2022-10-16 21:47:25 -0300

    #Layout icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
     <!-- Styles -->
-    <link href="{{ asset('css/dashlite.css?ver=3.0.3') }}" rel="stylesheet">
+    <link href="{{ asset('css/dashlite.css?ver=3.0.4') }}" rel="stylesheet">
 </head>
 
 @guest
