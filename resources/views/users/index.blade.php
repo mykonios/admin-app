cadbbc156f40ae3813a833414cf4dc9482911b64
antonio.mykonios <conexaovirtual_net@hotmail.com>
2022-10-17 17:08:06 -0300

    #Layout usuários

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,19 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Users Management</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-success" href="{{ route('users.create') }}"> Create New User</a>
-        </div>
-    </div>
-</div>
-
-
-
 
 <div class="nk-content" style="margin-top: 85px;">
     <div class="container-fluid">
