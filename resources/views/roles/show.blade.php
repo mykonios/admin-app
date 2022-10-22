527061c796826343b0e36bdd0683def66a16a8e9
antonio.mykonios <conexaovirtual_net@hotmail.com>
2022-10-22 19:26:40 -0300

    #Desenvolvimento Perfil do usuário

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -1,34 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2> Show Role</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('roles.index') }}"> Back</a>
-        </div>
-    </div>
-</div>
+
+@can('role-edit')
+    <div class="nk-content" style="margin-top: 85px;">
+        <div class="container-fluid">
+            <div class="nk-content-body">
 
 
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Name:</strong>
-            {{ $role->name }}
-        </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Name:</strong>
+                            {{ $role->name }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Permissions:</strong>
+                            @if(!empty($rolePermissions))
+                                @foreach($rolePermissions as $v)
+                                    <label class="label label-success">{{ $v->name }},</label>
+                                @endforeach
+                            @endif
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>     
+        </div>    
     </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Permissions:</strong>
-            @if(!empty($rolePermissions))
-                @foreach($rolePermissions as $v)
-                    <label class="label label-success">{{ $v->name }},</label>
-                @endforeach
-            @endif
-        </div>
-    </div>
-</div>
+@else
+    
+    header("Location: {{ route('home') }}");
+    die();    
+
+@endcan
+{!! $roles->render() !!}
 @endsection
