20483cd2d0b513cb44b0cfcab4e5840dceeab831
antonio.mykonios <conexaovirtual_net@hotmail.com>
2022-10-22 19:45:14 -0300

    #Desenvolvimento Perfil do usuário

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -3,34 +3,57 @@
 @section('content')
 
 @can('role-edit')
+
     <div class="nk-content" style="margin-top: 85px;">
         <div class="container-fluid">
             <div class="nk-content-body">
 
 
-                <div class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="form-group">
-                            <strong>Name:</strong>
-                            {{ $role->name }}
+                <div class="nk-block-head nk-block-head-sm">
+                    <div class="nk-block-between">
+                        <div class="nk-block-head-content">
+                            <h3 class="nk-block-title page-title">Visualizar Perfil</h3>
                         </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="form-group">
-                            <strong>Permissions:</strong>
-                            @if(!empty($rolePermissions))
-                                @foreach($rolePermissions as $v)
-                                    <label class="label label-success">{{ $v->name }},</label>
-                                @endforeach
-                            @endif
+                        <div class="nk-block-head-content">
+                            <div class="toggle-wrap nk-block-tools-toggle">
+                                <a href="#" class="btn btn-icon btn-trigger toggle-expand me-n1" data-target="pageMenu">
+                                    <em class="icon ni ni-more-v"></em>
+                                </a>
+                                <div class="toggle-expand-content" data-content="pageMenu">
+                                    <a class="btn btn-success" href="{{ route('roles.index') }}"> Voltar</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                
+                <div class="nk-block">
+                    <div class="row g-gs">
+
+                        <div class="col-xs-12 col-sm-12 col-md-12">
+                            <div class="form-group">
+                                <strong>Name:</strong>
+                                {{ $role->name }}
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12">
+                            <div class="form-group">
+                                <strong>Permissions:</strong>
+                                @if(!empty($rolePermissions))
+                                    @foreach($rolePermissions as $v)
+                                        <label class="label label-success">{{ $v->name }},</label>
+                                    @endforeach
+                                @endif
+                            </div>
+                        </div>
+
+                    </div>        
+                </div>
             </div>     
         </div>    
     </div>
+
+
 @else
     
     header("Location: {{ route('home') }}");
