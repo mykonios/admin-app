2a792fdcf793d4818082c3f69cb74d12f0bc1fdf
antonio.mykonios <conexaovirtual_net@hotmail.com>
2022-11-02 12:13:54 -0300

    #Desenvolvimento Perfil do usuário Migrations

--- database/migrations/2022_10_28_224530_alter_table_roles_20221028_0.php
+++ database/migrations/2022_10_28_224530_alter_table_roles_20221028_0.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('roles', function (Blueprint $table) {
             $table->integer('status_id')->nullable()->default(null)->comments('Status do Perfil do Usuário')->after('guard_name');
-            $table->integer('user_id')->nullable()->default(null)->comments('Usuário que efetuou a ação');
+            $table->integer('user_id')->nullable()->default(null)->comments('Usuário que efetuou a ação')->after('status_id');
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comments('Data exclusão do registro')->after('created_at')->change();
             $table->timestamp('deleted_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comments('Data exclusão do registro');
             $table->timestamp('date_audit')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comments('Data verificação da auditoria');
@@ -29,8 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('status_id');
-        Schema::dropIfExists('user_id');
         Schema::dropIfExists('deleted_at');
         Schema::dropIfExists('date_audit');
     }
